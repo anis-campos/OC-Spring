7369817cf26f2e65330e7744b3026f7ff5bf4ed8
Anis DA SILVA CAMPOS <yocorporation@hotmail.com>
2019-09-10 09:30:40 -0400

    Adding constructor with id

--- ticket/ticket-model/src/main/java/org/example/demo/ticket/model/bean/utilisateur/Utilisateur.java
+++ ticket/ticket-model/src/main/java/org/example/demo/ticket/model/bean/utilisateur/Utilisateur.java
@@ -6,16 +6,23 @@ public class Utilisateur {
     private String nom;
     private String prenom;
 
+    public Utilisateur(int responsable_id) {
+        this.id = responsable_id;
+    }
+
 
     public Integer getId() {
         return id;
     }
+
     public void setId(Integer pId) {
         id = pId;
     }
+
     public String getNom() {
         return nom;
     }
+
     public void setNom(String pNom) {
         nom = pNom;
     }
@@ -23,6 +30,7 @@ public class Utilisateur {
     public String getPrenom() {
         return prenom;
     }
+
     public void setPrenom(String pPrenom) {
         prenom = pPrenom;
     }
